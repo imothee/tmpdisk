de8b1279e563240ef23af70cde580ec210c33b89
Timothy Marks <t@imothee.com>
2018-08-22 23:24:06 -0700

    Fix issue with folders not being created on startup

--- TmpDisk/AppDelegate.m
+++ TmpDisk/AppDelegate.m
@@ -29,131 +29,131 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-  // Insert code here to initialize your application
-  
-  // Check if TmpDisk was launched with command line args
-  NSArray *arguments = [NSProcessInfo processInfo].arguments;
-  
-  NSString *argName = nil;
-  NSString *argSize = nil;
-  // TODO: There's likely a better way to parse command line args
-  for (NSUInteger i=0, n=arguments.count; i<n; i++) {
-    NSString *s = arguments[i];
-    
-    // We expect args in the format -argname=argval
-    
-    NSArray *arg = [s componentsSeparatedByString:@"="];
-    if (arg.count == 2) {
-      if ([arg[0] isEqualToString:@"-name"]) {
-        argName = [NSString stringWithString:arg[1]];
-      } else if ([arg[0] isEqualToString:@"-size"]) {
-        argSize = [NSString stringWithString:arg[1]];
-      }
-    }
-  }
-  
-  if (argName != nil && argSize != nil) {
-    
-    int dsize = argSize.intValue;
-    u_int64_t size = (((u_int64_t) dsize) * 1024 * 1024 / 512);
-    
-    if(![[NSFileManager defaultManager] fileExistsAtPath:[TmpDiskManager pathForName:argName] isDirectory:nil]) {
-      [TmpDiskManager createTmpDiskWithName:argName size:size autoCreate:NO indexed:NO hidden:NO folders:[[NSArray alloc] init] onSuccess:nil];
-    }
-    
-  }
+    // Insert code here to initialize your application
 
-  [TmpDiskManager autoCreateVolumesWithNames:nil];
+    // Check if TmpDisk was launched with command line args
+    NSArray *arguments = [NSProcessInfo processInfo].arguments;
+
+    NSString *argName = nil;
+    NSString *argSize = nil;
+    // TODO: There's likely a better way to parse command line args
+    for (NSUInteger i=0, n=arguments.count; i<n; i++) {
+        NSString *s = arguments[i];
+
+        // We expect args in the format -argname=argval
+
+        NSArray *arg = [s componentsSeparatedByString:@"="];
+        if (arg.count == 2) {
+            if ([arg[0] isEqualToString:@"-name"]) {
+                argName = [NSString stringWithString:arg[1]];
+            } else if ([arg[0] isEqualToString:@"-size"]) {
+                argSize = [NSString stringWithString:arg[1]];
+            }
+        }
+    }
+
+    if (argName != nil && argSize != nil) {
+
+        int dsize = argSize.intValue;
+        u_int64_t size = (((u_int64_t) dsize) * 1024 * 1024 / 512);
+
+        if(![[NSFileManager defaultManager] fileExistsAtPath:[TmpDiskManager pathForName:argName] isDirectory:nil]) {
+            [TmpDiskManager createTmpDiskWithName:argName size:size autoCreate:NO indexed:NO hidden:NO folders:[[NSArray alloc] init] onSuccess:nil];
+        }
+
+    }
+
+    [TmpDiskManager autoCreateVolumesWithNames:nil];
 }
 
 - (void)awakeFromNib {
-  
-  statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-  statusItem.menu = statusMenu;
-  statusItem.image = [NSImage imageNamed:@"status.png"];
-  [statusItem setHighlightMode:YES];
-  
-  [self newTmpDiskCreated:nil];
-  
-  
-  // Add a notification watcher to watch for disks added to refresh the menu
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(newTmpDiskCreated:) name:@"TmpDiskCreated" object:nil];
-  
-  [[NSWorkspace sharedWorkspace].notificationCenter addObserver:self selector:@selector(diskUnmounted:) name:NSWorkspaceDidUnmountNotification object:nil];
-  
+
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    statusItem.menu = statusMenu;
+    statusItem.image = [NSImage imageNamed:@"status.png"];
+    [statusItem setHighlightMode:YES];
+
+    [self newTmpDiskCreated:nil];
+
+
+    // Add a notification watcher to watch for disks added to refresh the menu
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(newTmpDiskCreated:) name:@"TmpDiskCreated" object:nil];
+
+    [[NSWorkspace sharedWorkspace].notificationCenter addObserver:self selector:@selector(diskUnmounted:) name:NSWorkspaceDidUnmountNotification object:nil];
+
 }
 
 - (void)diskUnmounted:(NSNotification *)notification {
-  
-  // Piggyback off a new disk created call when we hear the system unmounted a disk
-  // Clear the menu and redraw with al the valid volumes, sans any that were removed in finder or another app
-  [self newTmpDiskCreated:notification];
-  
+
+    // Piggyback off a new disk created call when we hear the system unmounted a disk
+    // Clear the menu and redraw with al the valid volumes, sans any that were removed in finder or another app
+    [self newTmpDiskCreated:notification];
+
 }
 
 - (void)newTmpDiskCreated:(NSNotification *)notification {
-  
-  NSError *e = nil;
-  NSArray *volumes = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:@"/Volumes" error:&e];
-  
-  if (e != nil) {
-    NSAlert *a = [NSAlert alertWithError:e];
-    [a runModal];
-    return;
-  }
-  
-  // We have a new disk so remove all the old ones and rebuild the menu
-  [diskMenu removeAllItems];
-  
-  for (NSString *s in volumes) {
-    
-    if (![[NSFileManager defaultManager] fileExistsAtPath:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", s]]) {
-      continue;
+
+    NSError *e = nil;
+    NSArray *volumes = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:@"/Volumes" error:&e];
+
+    if (e != nil) {
+        NSAlert *a = [NSAlert alertWithError:e];
+        [a runModal];
+        return;
     }
-    
-    NSMenuItem *mi = [[TmpDiskMenuItem alloc] initWithTitle:s action:@selector(tmpDiskSelected:) keyEquivalent:@""
-                                                 recreateBlock:^(NSString* s){
-      
-      // Recreate Block passed to menu to run when the recreate button is clicked for a TmpDisk
-      
-      [statusMenu cancelTrackingWithoutAnimation];
-      
-        [self ejectVolumeWithName:s recreate:YES];
-      
-      return;
+
+    // We have a new disk so remove all the old ones and rebuild the menu
+    [diskMenu removeAllItems];
+
+    for (NSString *s in volumes) {
+
+        if (![[NSFileManager defaultManager] fileExistsAtPath:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", s]]) {
+            continue;
+        }
+
+        NSMenuItem *mi = [[TmpDiskMenuItem alloc] initWithTitle:s action:@selector(tmpDiskSelected:) keyEquivalent:@""
+                                                  recreateBlock:^(NSString* s){
+
+                                                      // Recreate Block passed to menu to run when the recreate button is clicked for a TmpDisk
+
+                                                      [statusMenu cancelTrackingWithoutAnimation];
+
+                                                      [self ejectVolumeWithName:s recreate:YES];
+
+                                                      return;
+
+                                                  }
+                                                     ejectBlock:^(NSString* s){
+
+                                                         // Eject Block passed to menu to run when the eject button is clicked for a TmpDisk
+
+                                                         [statusMenu cancelTrackingWithoutAnimation];
+
+                                                         [self ejectVolumeWithName:s recreate:NO];
+
+                                                         return;
+
+                                                     }
+                          ];
+
+        [diskMenu addItem:mi];
 
     }
-                                                 ejectBlock:^(NSString* s){
-      
-      // Eject Block passed to menu to run when the eject button is clicked for a TmpDisk
-      
-      [statusMenu cancelTrackingWithoutAnimation];
-      
-        [self ejectVolumeWithName:s recreate:NO];
-      
-      return;
-
-    }
-                      ];
-
-    [diskMenu addItem:mi];
-
-  }
 
 }
 
 - (void)ejectVolumeWithName:(NSString *)name recreate:(BOOL)recreate {
-  [TmpDiskManager ejectVolumesWithNames:[NSSet setWithObject:name]
-                               recreate:recreate];
+    [TmpDiskManager ejectVolumesWithNames:[NSSet setWithObject:name]
+                                 recreate:recreate];
 }
 
 - (void)tmpDiskSelected:(id)sender {
 
-  // When a tmpDisk menu option is selected, we open the volume in Finder
+    // When a tmpDisk menu option is selected, we open the volume in Finder
 
-  NSString *s = [sender title];
+    NSString *s = [sender title];
 
-  [TmpDiskManager openVolumeWithName:s];
+    [TmpDiskManager openVolumeWithName:s];
 }
 
 @end
--- TmpDisk/Classes/TmpDiskManager.m
+++ TmpDisk/Classes/TmpDiskManager.m
@@ -25,9 +25,9 @@
 @implementation TmpDiskFile
 
 - (BOOL)exists {
-  return [[NSFileManager defaultManager]
-      fileExistsAtPath:[TmpDiskManager pathForName:self.name]
-           isDirectory:nil];
+    return [[NSFileManager defaultManager]
+            fileExistsAtPath:[TmpDiskManager pathForName:self.name]
+            isDirectory:nil];
 }
 
 @end
@@ -35,256 +35,265 @@
 @implementation TmpDiskManager
 
 + (NSString *)pathForName:(NSString *)name {
-  return [NSString stringWithFormat:@"/Volumes/%@", name];
+    return [NSString stringWithFormat:@"/Volumes/%@", name];
 }
 
 + (void)autoCreateVolumesWithNames:(NSSet<NSString *> *)names {
 
-  for (TmpDiskFile *disk in [self knownVolumesWithNames:names]) {
-    if (disk.exists) {
-      continue;
-    }
+    for (TmpDiskFile *disk in [self knownVolumesWithNames:names]) {
+        if (disk.exists) {
+            continue;
+        }
 
-    [self createTmpDiskWithName:disk.name
-                           size:disk.size
-                     autoCreate:NO
-                        indexed:disk.indexed
-                         hidden:disk.hidden
-                        folders:disk.folders
-                      onSuccess:nil];
-  }
+        [self createTmpDiskWithName:disk.name
+                               size:disk.size
+                         autoCreate:NO
+                            indexed:disk.indexed
+                             hidden:disk.hidden
+                            folders:disk.folders
+                          onSuccess:nil];
+    }
 }
 
 + (NSArray<TmpDiskFile *> *)knownVolumesWithNames:(NSSet<NSString *> *)names {
 
-  NSMutableArray *results = [[NSMutableArray alloc] init];
+    NSMutableArray *results = [[NSMutableArray alloc] init];
 
-  if ([[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]) {
+    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]) {
 
-    NSArray *autoCreateArray =
+        NSArray *autoCreateArray =
         [[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"];
 
-    for (NSDictionary *d in autoCreateArray) {
+        for (NSDictionary *d in autoCreateArray) {
 
-      NSString *name = d[@"name"];
+            NSString *name = d[@"name"];
 
-      if (names.count && ![names containsObject:name]) {
-        continue;
-      }
+            if (names.count && ![names containsObject:name]) {
+                continue;
+            }
 
-      NSNumber *size = d[@"size"];
+            NSNumber *size = d[@"size"];
 
-      if (name && size) {
-        TmpDiskFile *disk = [[TmpDiskFile alloc] init];
-        disk.name = name;
-        disk.size = size.unsignedLongLongValue;
-        disk.indexed = [d[@"indexed"] boolValue];
-        disk.hidden = [d[@"hidden"] boolValue];
-        disk.folders = [d objectForKey:@"folders"] ?: @[];
-        [results addObject:disk];
-      }
+            if (name && size) {
+                TmpDiskFile *disk = [[TmpDiskFile alloc] init];
+                disk.name = name;
+                disk.size = size.unsignedLongLongValue;
+                disk.indexed = [d[@"indexed"] boolValue];
+                disk.hidden = [d[@"hidden"] boolValue];
+                disk.folders = [d objectForKey:@"folders"] ?: @[];
+                [results addObject:disk];
+            }
+        }
     }
-  }
 
-  return results;
+    return results;
 }
 
 + (void)ejectVolumesWithNames:(NSSet<NSString *> *)names
                      recreate:(BOOL)recreate {
-  dispatch_group_t group = dispatch_group_create();
+    dispatch_group_t group = dispatch_group_create();
 
-  for (TmpDiskFile *disk in [self knownVolumesWithNames:names]) {
-    dispatch_group_enter(group);
-    NSString *volumePath = [self pathForName:disk.name];
+    for (TmpDiskFile *disk in [self knownVolumesWithNames:names]) {
+        dispatch_group_enter(group);
+        NSString *volumePath = [self pathForName:disk.name];
 
-    BOOL isRemovable, isWritable, isUnmountable;
-    NSString *description, *type;
+        BOOL isRemovable, isWritable, isUnmountable;
+        NSString *description, *type;
 
-    NSWorkspace *ws = [[NSWorkspace alloc] init];
+        NSWorkspace *ws = [[NSWorkspace alloc] init];
 
-    // Make sure the Volume is unmountable first
-    [ws getFileSystemInfoForPath:volumePath
-                     isRemovable:&isRemovable
-                      isWritable:&isWritable
-                   isUnmountable:&isUnmountable
-                     description:&description
-                            type:&type];
-    if (isUnmountable) {
-      [ws unmountAndEjectDeviceAtPath:volumePath];
+        // Make sure the Volume is unmountable first
+        [ws getFileSystemInfoForPath:volumePath
+                         isRemovable:&isRemovable
+                          isWritable:&isWritable
+                       isUnmountable:&isUnmountable
+                         description:&description
+                                type:&type];
+        if (isUnmountable) {
+            [ws unmountAndEjectDeviceAtPath:volumePath];
 
-      if (recreate) {
-        [self autoCreateVolumesWithNames:[NSSet setWithObject:disk.name]];
-      }
+            if (recreate) {
+                [self autoCreateVolumesWithNames:[NSSet setWithObject:disk.name]];
+            }
+        }
+        dispatch_group_leave(group);
     }
-    dispatch_group_leave(group);
-  }
 }
 
 + (void)openVolumeWithName:(NSString *)name {
 
-  NSString *volumePath = [self pathForName:name];
+    NSString *volumePath = [self pathForName:name];
 
-  NSWorkspace *ws = [[NSWorkspace alloc] init];
+    NSWorkspace *ws = [[NSWorkspace alloc] init];
 
-  [ws openFile:volumePath];
+    [ws openFile:volumePath];
 }
 
 + (bool)createTmpDiskWithName:(NSString*)name size:(u_int64_t)size autoCreate:(bool)autoCreate indexed:(bool)indexed hidden:(bool)hidden folders:(NSArray*)folders onSuccess:(void (^)())success {
-  
-  if (name.length == 0) {
-    NSAlert *a = [NSAlert alertWithMessageText:@"Error Creating TmpDisk" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"You must provide a Disk Name"];
-    [a runModal];
-    
-    return NO;
-  }
-  
-  if ([[NSFileManager defaultManager] fileExistsAtPath:[self pathForName:name] isDirectory:nil]) {
-    NSAlert *a = [NSAlert alertWithMessageText:@"Error Creating TmpDisk" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"A Volume named %@ already exists.", name];
-    [a runModal];
-    
-    return NO;
-  }
-  
-  // Check if the disk has been set to auto-create on startup
-  if (autoCreate) {
-    
-    // Add the volume to autoCreate if it doesn't already exist in there
-    
-    NSMutableArray *autoCreateArray;
-    
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]) {
-      // Existing copy in user defaults
-      
-      autoCreateArray = [NSMutableArray arrayWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]];
-      
+
+    if (name.length == 0) {
+        NSAlert *a = [NSAlert alertWithMessageText:@"Error Creating TmpDisk" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"You must provide a Disk Name"];
+        [a runModal];
+
+        return NO;
+    }
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[self pathForName:name] isDirectory:nil]) {
+        NSAlert *a = [NSAlert alertWithMessageText:@"Error Creating TmpDisk" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"A Volume named %@ already exists.", name];
+        [a runModal];
+
+        return NO;
+    }
+
+    // Check if the disk has been set to auto-create on startup
+    if (autoCreate) {
+
+        // Add the volume to autoCreate if it doesn't already exist in there
+
+        NSMutableArray *autoCreateArray;
+
+        if ([[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]) {
+            // Existing copy in user defaults
+
+            autoCreateArray = [NSMutableArray arrayWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"autoCreate"]];
+
+        } else {
+            autoCreateArray = [NSMutableArray array];
+        }
+
+        // Holds the current disk values to replicate
+        NSMutableDictionary *curDisk = [NSMutableDictionary dictionary];
+        curDisk[@"name"] = name;
+        curDisk[@"size"] = @(size);
+        curDisk[@"indexed"] = @(indexed);
+        curDisk[@"hidden"] = @(hidden);
+        curDisk[@"folders"] = folders;
+
+        // Check whether same name exists in autoCreate already
+        bool exists = false;
+        for (NSDictionary *d in autoCreateArray) {
+            if ([d[@"name"] compare:name options:NSCaseInsensitiveSearch] == NSOrderedSame) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            // Doesn't exist so we're safe to add it to the list
+            [autoCreateArray addObject:curDisk];
+
+            [[NSUserDefaults standardUserDefaults] setObject:autoCreateArray forKey:@"autoCreate"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+
+    }
+
+    // We need a task to run the sys call to create a new tmpdisk volume
+
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    task.launchPath = @"/bin/sh";
+
+    NSString *command;
+    if (hidden) {
+        NSString *pattern = @"d=$(hdiutil attach -nomount ram://%llu) && diskutil eraseDisk HFS+ %%noformat%% $d && "
+        @"newfs_hfs -v \"%@\" \"$(echo $d | tr -d ' ')s1\" && hdiutil attach -nomount $d && "
+        @"hdiutil attach -nobrowse \"$(echo $d | tr -d ' ')s1\"";
+
+        command = [NSString stringWithFormat:pattern, size, name];
     } else {
-      autoCreateArray = [NSMutableArray array];
+        command = [NSString stringWithFormat:@"diskutil erasevolume HFS+ \"%@\" `hdiutil attach -nomount ram://%llu`", name, size];
     }
-    
-    // Holds the current disk values to replicate
-    NSMutableDictionary *curDisk = [NSMutableDictionary dictionary];
-    curDisk[@"name"] = name;
-    curDisk[@"size"] = @(size);
-    curDisk[@"indexed"] = @(indexed);
-    curDisk[@"hidden"] = @(hidden);
-    curDisk[@"folders"] = folders;
-    
-    // Check whether same name exists in autoCreate already
-    bool exists = false;
-    for (NSDictionary *d in autoCreateArray) {
-      if ([d[@"name"] compare:name options:NSCaseInsensitiveSearch] == NSOrderedSame) {
-        exists = true;
-        break;
-      }
+
+    NSArray *arguments = @[@"-c", command];
+
+    task.arguments = arguments;
+
+    NSTask *indexTask = nil;
+    if (indexed) {
+        // If the indexed flag was set we need to run another task to index the drive
+        indexTask = [[NSTask alloc] init];
+        indexTask.launchPath = @"/bin/sh";
+
+        NSArray *arguments;
+        arguments = @[@"-c",
+                      [NSString stringWithFormat:@"mdutil -i on %@", [self pathForName:name]]];
+
+        indexTask.arguments = arguments;
     }
-    
-    if (!exists) {
-      // Doesn't exist so we're safe to add it to the list
-      [autoCreateArray addObject:curDisk];
-      
-      [[NSUserDefaults standardUserDefaults] setObject:autoCreateArray forKey:@"autoCreate"];
-      [[NSUserDefaults standardUserDefaults] synchronize];
-    }
-    
-  }
-  
-  // We need a task to run the sys call to create a new tmpdisk volume
-  
-  NSTask *task;
-  task = [[NSTask alloc] init];
-  task.launchPath = @"/bin/sh";
-  
-  NSString *command;
-  if (hidden) {
-    NSString *pattern = @"d=$(hdiutil attach -nomount ram://%llu) && diskutil eraseDisk HFS+ %%noformat%% $d && "
-    @"newfs_hfs -v \"%@\" \"$(echo $d | tr -d ' ')s1\" && hdiutil attach -nomount $d && "
-    @"hdiutil attach -nobrowse \"$(echo $d | tr -d ' ')s1\"";
-    
-    command = [NSString stringWithFormat:pattern, size, name];
-  } else {
-    command = [NSString stringWithFormat:@"diskutil erasevolume HFS+ \"%@\" `hdiutil attach -nomount ram://%llu`", name, size];
-  }
-  
-  NSArray *arguments = @[@"-c", command];
-  
-  task.arguments = arguments;
-  
-  NSTask *indexTask = nil;
-  if (indexed) {
-    // If the indexed flag was set we need to run another task to index the drive
-    indexTask = [[NSTask alloc] init];
-    indexTask.launchPath = @"/bin/sh";
-    
-    NSArray *arguments;
-    arguments = @[@"-c",
-                  [NSString stringWithFormat:@"mdutil -i on %@", [self pathForName:name]]];
-    
-    indexTask.arguments = arguments;
-  }
-  
+
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_7
-  // Check for termination handler existences
-  
-  [task setTerminationHandler:^(NSTask* task) {
-    
-    
-    NSDictionary *tmpProps = [NSDictionary dictionaryWithObjects:
-                              [NSArray arrayWithObjects:[NSNumber numberWithBool:NO], nil] forKeys:
-                              [NSArray arrayWithObjects:@"backup", nil]];
-    
-    [tmpProps writeToFile:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", name] atomically:YES];
-    
-    if (indexTask != nil) {
-      [indexTask launch];
-    }
-    
-    if (success != nil) {
-      success();
-    }
-    
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"TmpDiskCreated" object:name];
-    
-    
-  }];
-  
-  [task launch];
-  
+    // Check for termination handler existences
+
+    [task setTerminationHandler:^(NSTask* task) {
+
+
+        NSDictionary *tmpProps = [NSDictionary dictionaryWithObjects:
+                                  [NSArray arrayWithObjects:[NSNumber numberWithBool:NO], nil] forKeys:
+                                  [NSArray arrayWithObjects:@"backup", nil]];
+
+        [tmpProps writeToFile:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", name] atomically:YES];
+
+        if (indexTask != nil) {
+            [indexTask launch];
+        }
+
+        [self createFolders:name folders:folders];
+
+        if (success != nil) {
+            success();
+        }
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"TmpDiskCreated" object:name];
+        });
+
+    }];
+
+    [task launch];
+
 #else
-  
-  [task launch];
-  [task waitUntilExit];
-  
-  NSDictionary *tmpProps = @{@"backup": @NO};
-  
-  [tmpProps writeToFile:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", name] atomically:YES];
-  
-  if (indexTask != nil) {
-    [indexTask launch];
-  }
-  
-  if (success != nil) {
-    success();
-  }
-  
-  [[NSNotificationCenter defaultCenter] postNotificationName:@"TmpDiskCreated" object:name];
-  
-#endif
-  
-  // Handle the folder creation
-  
-  for(id f in folders) {
-    NSString *folder = (NSString*)f;
-    NSString *dir = [NSString stringWithFormat:@"/Volumes/%@/%@", name, folder];
-    
-    NSError *error = nil;
-    BOOL isDir = YES;
-    if (![[NSFileManager defaultManager] fileExistsAtPath:dir isDirectory:&isDir]) {
-      [[NSFileManager defaultManager] createDirectoryAtPath:dir withIntermediateDirectories:YES attributes:nil error:&error];
-      
+
+    [task launch];
+    [task waitUntilExit];
+
+    NSDictionary *tmpProps = @{@"backup": @NO};
+
+    [tmpProps writeToFile:[NSString stringWithFormat:@"/Volumes/%@/.tmpdisk", name] atomically:YES];
+
+    if (indexTask != nil) {
+        [indexTask launch];
     }
-  }
-  
-  return YES;
+
+    [self createFolders:name folders:folders];
+
+    if (success != nil) {
+        success();
+    }
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"TmpDiskCreated" object:name];
+
+#endif
+
+    return YES;
+}
+
++ (void)createFolders:(NSString*)name folders:(NSArray*)folders {
+    // Handle the folder creation
+
+    for(id f in folders) {
+        NSString *folder = (NSString*)f;
+        NSString *dir = [NSString stringWithFormat:@"/Volumes/%@/%@", name, folder];
+
+        NSError *error = nil;
+        BOOL isDir = YES;
+        if (![[NSFileManager defaultManager] fileExistsAtPath:dir isDirectory:&isDir]) {
+            [[NSFileManager defaultManager] createDirectoryAtPath:dir withIntermediateDirectories:YES attributes:nil error:&error];
+            if (error != nil) {
+                NSLog(@"%@", error);
+            }
+        }
+    }
 }
 
 @end
