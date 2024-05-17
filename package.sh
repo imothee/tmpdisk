221ad1427c89b2d1e46e3306198eb5a1a4e42950
Tim Marks <t@imothee.com>
2024-05-17 11:13:21 -0700

    fix(package) adds the sparkle process to package.sh and mvoes to npx instead of a package.json file

--- package.sh
+++ package.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo "Ensure you have the latest appdmg installed"
-echo "npm install -g appdmg"
+# Ensure the build directory exists
+[ ! -d "./build" ] && mkdir "./build"
 
-npx appdmg ./appdmg.json ./TmpDisk.dmg
+# Ensure we have a TmpDisk.app in the folder
+if [ ! -f "./TmpDisk.app" ]; then
+    echo "Error: TmpDisk.app not found in the current directory"
+    exit 1
+fi
+
+# Package the TmpDisk.app into the dmg
+npx appdmg@latest appdmg ./appdmg.json ./build/TmpDisk.dmg
+
+# Find the sparkle-project folder
+#sparkle = ls -t "~/Library/Developer/Xcode/DerivedData/TmpDisk-" | head -1
+sparkle=$(find ~/Library/Developer/Xcode/DerivedData -type d -maxdepth 1 -print | grep -m1 'TmpDisk-')
+
+# Sign the dmg
+"$sparkle"/SourcePackages/artifacts/sparkle/bin/generate_appcast -f ./keys/dsa_priv.pem ./build
